cc8810e0c0f43e7ed1b4af7492e841bbf44b8bf4
losba78rf2 <randomlbpstuff@gmail.com>
2025-12-28 20:43:10 +1100

    smal upfdate

--- ggg/popit/popitbutton.gd
+++ ggg/popit/popitbutton.gd
@@ -9,12 +9,17 @@ func _ready() -> void:
 	
 func changePP():
 	$"../..".ChangeTo = self.PopitPage
+	$"../../Highlight".hide()
 func _process(_delta: float) -> void:
 	if self.is_hovered():
+		$"../../Highlight".show()
 		$"../../Highlight/HighLight".destination = self.global_position
+		self.scale = lerp(self.scale, Vector2(1.15, 1.15), 0.25)
 		$"../../Highlight/PanelContainer".show()
 		$"../../Highlight/PanelContainer/HighlightName".text = self.name
 		if self.is_in_group("SmallerButton"):
 			$"../../Highlight/HighLight".size = lerp($"../../Highlight/HighLight".size, Vector2(40, 40), 0.25)
 		if self.is_in_group("MainButton"):
 			$"../../Highlight/HighLight".size = lerp($"../../Highlight/HighLight".size, Vector2(54, 54), 0.25)
+	else:
+		self.scale = lerp(self.scale, Vector2(1.0, 1.0), 0.25)
